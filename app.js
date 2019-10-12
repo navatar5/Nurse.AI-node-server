7ad52cc76938567eb8433284f5fca3b2395efdc5
Pranesh Navarathna <pranesh.navarathna@gmail.com>
2019-10-12 12:24:23 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+debugger
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const product = require('./routes/product.route'); // Imports routes for the products
+
+// initialize our express app
+const app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb+srv://pranesh94:abcd1234@cluster0-frs5l.mongodb.net/test?retryWrites=true&w=majority';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/products', product);
+
+let port = 1234;
+
+app.listen(port, () => {
+    console.log('Server is up and running on port numner ' + port);
+});
