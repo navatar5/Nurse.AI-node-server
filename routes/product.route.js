25420926583c42be4fc1b1812cf554750d369ab2
Pranesh Navarathna <pranesh.navarathna@gmail.com>
2019-10-12 15:10:59 -0400

    Changed to mLab DB and it worksgit add .

--- routes/product.route.js
+++ routes/product.route.js
@@ -14,3 +14,5 @@ router.post('/create', product_controller.product_create);
 router.get('/:id', product_controller.product_details);
 
 router.put('/:id/update', product_controller.product_update);
+
+router.get(/alldata, product_controller.alldata))
