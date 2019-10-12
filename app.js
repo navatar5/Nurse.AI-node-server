e93c77769533cd4de490ec9441fa2a3d49f0c70a
Pranesh Navarathna <pranesh.navarathna@gmail.com>
2019-10-12 14:37:39 -0400

    attempt 2

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const app = express();
 // Set up mongoose connection
 const mongoose = require('mongoose');
 let dev_db_url = 'mongodb+srv://pranesh94:abcd1234@cluster0-frs5l.mongodb.net/test?retryWrites=true&w=majority';
-let mongoDB = process.env.MONGODB_URI || dev_db_url;
+// let mongoDB = process.env.MONGODB_URI || dev_db_url;
+let mongoDB = dev_db_url;
 mongoose.connect(mongoDB, {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
